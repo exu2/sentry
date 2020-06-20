2b11b8e384fc6ed9d7c9c07f223732d5ef3935b3
ethanxuyizhou <xuyizhou98@gmail.com>
2020-06-20 17:03:30 -0400

    z

--- src/lib/cryptography.ml
+++ src/lib/cryptography.ml
@@ -2,10 +2,24 @@ open! Core
 open! Async
 open! Import
 
-(* TODO: This needs fixing *)
+let trim_or_pad string =
+  match String.length string with
+  | 32 -> string
+  | length when length < 32 ->
+      let pad_string =
+        List.map (List.range 0 (32 - length)) ~f:(fun _ -> " ")
+        |> String.concat ~sep:""
+      in
+      String.concat ~sep:"" [ string; pad_string ]
+  | _ -> String.sub ~pos:0 ~len:32 string
+
 let cipher ~direction ~key ~data =
+  (* TODO: just a hack to make it work. Need to be fixed *)
+  let key = trim_or_pad key in
+  let data = trim_or_pad data in
   let transform = Cipher.aes key direction in
   transform#put_string data;
+  transform#finish;
   transform#get_string
 
 let encrypt ~key ~data = cipher ~direction:Cipher.Encrypt ~key ~data
