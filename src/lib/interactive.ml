fa3efede49cca8785c5fd785168eefa761d158b9
ethanxuyizhou <xuyizhou98@gmail.com>
2020-06-20 22:51:20 -0400

    z

--- src/lib/interactive.ml
+++ src/lib/interactive.ml
@@ -1,4 +1,8 @@
 open! Core
 open! Async
 
-let ask_user str = Async_interactive.ask_dispatch_gen ~f:(fun x -> Ok x) str
+let ask_user str =
+  Async_interactive.ask_dispatch_gen
+    ~f:(fun str ->
+      if String.is_empty str then Error "Answer should not be empty" else Ok str)
+    str
